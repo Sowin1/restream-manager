80b573cf0609ba814b891beaf0300d2024c8abe1
Sowin1 <bplsowin@outlook.fr>
2026-06-11 23:48:55 +0200

    feat: add stream and status controllers and routes

---
+++ src/controllers/status.controller.ts
@@ -0,0 +1,5 @@
+export class StatusController {
+    public streamStatus(){
+        // calling stream status service
+    }
+}
---
+++ src/controllers/stream.controller.ts
@@ -0,0 +1,9 @@
+export class StreamController {
+
+    public streamStart(){
+        // Calling starting stream service
+    }
+    public streamStop(){
+        // Calling stop stream service
+    }
+}
